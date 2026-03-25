3979ed40e70652b9c3b53f758444b35353ef196d
Ahmed Mohamed <a2004256@gmail.com>
2026-03-25 21:29:23 +0200

    Update main.kt

--- composeApp/src/jsMain/kotlin/main.kt
+++ composeApp/src/jsMain/kotlin/main.kt
@@ -13,7 +13,7 @@ fun main() {
     Firebase.initialize(
         options = FirebaseOptions(
             applicationId = "1:341932665810:android:53f1796e496878afdeb60b",
-            apiKey = "AIzaSyCdgy1iQLpQxYN03d7-_aWolTEcV_oigdg",
+            apiKey = "",
             projectId = "inputdata-48d12"
         )
     )
